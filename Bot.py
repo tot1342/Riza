c67eae0614f5e51d54af12050747d6106a1db217
tot1342 <kotlyarow1999@mail.ru>
2018-09-24 18:33:57 +0300

    Сделал основу для заполнения БД

--- Bot.py
+++ Bot.py
@@ -1,12 +1,22 @@
 import smtplib
+import sqlite3
+
 
 class Bot(object):
     def __init__(self):
         pass
 
-    def mail_with_error(self, error: str):
+    def mail_with_error(self, error: str): #отправляет мейл с ошибкой
         smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
         smtpObj.starttls()
         smtpObj.login('botforerror@mail.ru','12345bot')
         smtpObj.sendmail("botforerror@mail.ru","kotlyarow1999@mail.ru",str(error))
         smtpObj.quit()
+    def BD(self, operation: str, coin: str,amount: float,date: str):
+        conn = sqlite3.connect("history.db")
+        cursor = conn.cursor()
+        cursor.executemany("INSERT INTO history VALUES (?,?,?,?)", [(operation, coin, amount, date)])
+        conn.commit()
+
+        conn.close()
+
